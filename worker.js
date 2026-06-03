9da881f548440ede4f533b8683d042e5f455dba3
Martin Levy <mahtin@mahtin.com>
2026-06-03 14:43:10 -0700

    worker name change, 404 code added, extra domains added

--- worker.js
+++ worker.js
@@ -1,9 +1,19 @@
 export default {
   async fetch(request, env) {
+    // let's make all the domains move to one place...
+    const url = new URL(request.url)
+    switch (url.hostname) {
+      case 'ruzena.uk':
+      case 'ruzena.co.uk':
+      case 'rachellevy.uk':
+        url.hostname = 'rachellevy.org.uk'
+        return Response.redirect(url.toString(), 302)
+    }
+    // and now service the domain...
     try {
       return await env.ASSETS.fetch(request)
     } catch (err) {
-      return new Response("Not found", { status: 404 })
+      return new Response('Not found', { status: 404 })
     }
   }
 }
